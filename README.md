da6b2999303dbdae9ebda9bad3d36492e667d867
Kyle Hensman <kylehensman@Nancys-MacBook.lan>
2025-06-30 10:46:52 -0300

    feat: add animations and code cleanup

--- README.md
+++ README.md
@@ -8,10 +8,6 @@
 
 [Astro Themes](https://astro.build/themes/details/dark-minimal/)
 
-[ReactBits Showcase](https://www.reactbits.dev/showcase) 
-
-The component `<LetterGlitch \>` was taken from [ReactBits.dev](https://www.reactbits.dev/)
-
 ## **Stack**  
 ### **Frontend**  
 ![Astro](https://img.shields.io/badge/Astro-FF5D01?logo=astro&logoColor=white)
@@ -22,17 +18,6 @@ The component `<LetterGlitch \>` was taken from [ReactBits.dev](https://www.reac
 ![Figma](https://img.shields.io/badge/Figma-F24E1E?logo=figma&logoColor=white)
 ![Prettier](https://img.shields.io/badge/Prettier-F7B93E?logo=prettier&logoColor=black)
 ![Canva](https://img.shields.io/badge/Canva-c900c3?logo=canva&logoColor=white)
-
-### **Show your favorite Spotify album (or your own)** ![Spotify](https://img.shields.io/badge/Spotify-06cc1a?logo=spotify&logoColor=white)
-1. Choose your Spotify album
-2. Access the share options
-3. Select 'copy embed code'
-```
-<iframe src="https://open.spotify.com/embed/album/ALBUM_ID" style="border-radius:12px border:0;" class="w-full h-40" frameborder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
-```
-4. Insert the embed code on footer.astro
-
-That's it!
 
 ## **Project structure**
 ```
@@ -49,7 +34,6 @@ src/
 ├── layouts/
 |    └── Layout.astro
 ├── React/
-|    ├── LetterGlitch.tsx
 |    ├── LikeButton.tsx
 |    └── SkillsList.tsx
 └── pages/
--- src/React/SkillsList.tsx
+++ src/React/SkillsList.tsx
@@ -1,40 +1,13 @@
-import React, { useState } from "react";
-
-const CategoryIcons = {
-  "Web Development": (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      className="w-6 h-6 text-[var(--sec)] opacity-70"
-    >
-      <path d="M21 3C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3H21ZM20 11H4V19H20V11ZM20 5H4V9H20V5ZM11 6V8H9V6H11ZM7 6V8H5V6H7Z"></path>
-    </svg>
-  ),
-  "Mobile Development": (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      className="w-6 h-6 text-[var(--sec)] opacity-70"
-    >
-      <path d="M7 4V20H17V4H7ZM6 2H18C18.5523 2 19 2.44772 19 3V21C19 21.5523 18.5523 22 18 22H6C5.44772 22 5 21.5523 5 21V3C5 2.44772 5.44772 2 6 2ZM12 17C12.5523 17 13 17.4477 13 18C13 18.5523 12.5523 19 12 19C11.4477 19 11 18.5523 11 18C11 17.4477 11.4477 17 12 17Z"></path>
-    </svg>
-  ),
-  "UI/UX Design": (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      className="w-6 h-6 text-[var(--sec)] opacity-70"
-    >
-      <path d="M5.7646 7.99998L5.46944 7.26944C5.26255 6.75737 5.50995 6.17454 6.02202 5.96765L15.2939 2.22158C15.8059 2.01469 16.3888 2.26209 16.5956 2.77416L22.2147 16.6819C22.4216 17.194 22.1742 17.7768 21.6622 17.9837L12.3903 21.7298C11.8783 21.9367 11.2954 21.6893 11.0885 21.1772L11.0002 20.9586V21H7.00021C6.44792 21 6.00021 20.5523 6.00021 20V19.7303L2.65056 18.377C2.13849 18.1701 1.89109 17.5873 2.09798 17.0752L5.7646 7.99998ZM8.00021 19H10.2089L8.00021 13.5333V19ZM6.00021 12.7558L4.32696 16.8972L6.00021 17.6084V12.7558ZM7.69842 7.44741L12.5683 19.5008L19.9858 16.5039L15.1159 4.45055L7.69842 7.44741ZM10.6766 9.47974C10.1645 9.68663 9.5817 9.43924 9.37481 8.92717C9.16792 8.4151 9.41532 7.83227 9.92739 7.62538C10.4395 7.41849 11.0223 7.66588 11.2292 8.17795C11.4361 8.69002 11.1887 9.27286 10.6766 9.47974Z"></path>
-    </svg>
-  ),
-};
+import React, { useEffect, useState } from "react";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const SkillsList = () => {
   const [openItem, setOpenItem] = useState<string | null>(null);
+
+  useEffect(() => {
+    AOS.init();
+  }, []);
 
   const software = {
     "Web Development": [
@@ -60,15 +33,15 @@ const SkillsList = () => {
       "Security reviews & auditing",
       "NFT minting & DeFi integrations",
     ],
-    "RPC Management": [
-      "RPC fine-tuning & maintenance",
-      "Networking, optimization & scalability",
-      "Monitoring & advanced alert systems"
-    ],
     "Wallets and dApps": [
       "Design and develop Web3 wallets",
       "dApp integrations & development",
       "Improvements & optimizations",
+    ],
+    "RPC Management": [
+      "RPC fine-tuning & maintenance",
+      "Networking, optimization & scalability",
+      "Monitoring & advanced alert systems"
     ],
   };
 
@@ -78,15 +51,15 @@ const SkillsList = () => {
       "Secure and reliable deployments",
       "Cost reductions and assessments",
     ],
-    "Full System Observability": [
-      "Industry leading service monitoring",
-      "Tailtored alerts and messaging",
-      "Advanced disaster recovery planning",      
-    ],
-    "Automation and Productivity": [
+    "Automation and Tooling": [
       "Advanced scripting & automation",
       "Professional workflow optimizations",
       "Software stability & security testing",
+    ],
+    "System Observability": [
+      "Industry leading service monitoring",
+      "Tailtored alerts and messaging",
+      "Advanced disaster recovery planning",      
     ],
   };
 
@@ -96,27 +69,46 @@ const SkillsList = () => {
 
   return (
     <div className="text-left pt-3 md:pt-9">
-      <div className="my-16 md:my-20 mx-auto text-center">
+      <div
+        className="my-10 md:my-12 mx-auto text-center"
+        data-aos="fade-zoom-in"
+        data-aos-easing="ease-in-back"
+        data-aos-delay="0"
+        data-aos-offset="0"
+      >
         <h2 className="text-[var(--white)] text-3xl md:text-4xl font-semibold mb-2">
           Skills & Proficiencies
         </h2>
         <div className="bg-[#a476ff] w-[300px] mx-auto h-[2px]" />
       </div>
-      <div className="flex flex-col md:flex-row justify-around items-start gap-10">
-        <div>
-          <h4 className="text-[var(--white)] font-bold text-lg md:text-xl md:mb-6">Software</h4>
+      {/* <p
+        className="text-center max-w-[800px] w-full mx-auto text-md md:text-2xl text-[var(--white-icon)] mb-10 md:mb-12"
+        data-aos="fade-down"
+        data-aos-delay="200"
+        >
+          With experience in Frontend, Backend, and Mobile App Development, I excel at
+          bridging technical complexity with business objectives.
+      </p> */}
+      {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center items-start gap-10"> */}
+      <div className="flex flex-col md:flex-row flex-wrap justify-center gap-12">
+        <div
+          data-aos="fade-zoom-in"
+          data-aos-easing="ease-in-back"
+          data-aos-delay="400"
+          data-aos-offset="0"
+        >
+          <h4 className="text-[var(--white)] font-bold text-lg text-left md:text-xl md:mb-4">Software</h4>
           <ul className="space-y-4 mt-4 text-lg">
             {Object.entries(software).map(([category, items]) => (
               <li key={category} className="w-full">
                 <div
                   onClick={() => toggleItem(category)}
-                  className="md:w-[300px] w-full bg-[#1414149c] rounded-2xl text-left hover:bg-opacity-80 transition-all border border-[var(--white-icon-tr)] cursor-pointer overflow-hidden"
+                  className="w-full bg-[#1414149c] rounded-2xl text-left hover:bg-opacity-80 transition-all border border-[var(--white-icon-tr)] cursor-pointer overflow-hidden"
                 >
                   <div className="flex items-center gap-3 p-4">
-                    {CategoryIcons[category]}
                     <div className="flex items-center gap-2 flex-grow justify-between">
                       <div className="min-w-0 max-w-[200px] md:max-w-none overflow-hidden">
-                        <span className="block truncate text-[var(--white)] text-lg">
+                        <span className="block truncate text-[var(--white)] text-sm">
                           {category}
                         </span>
                       </div>
@@ -155,20 +147,24 @@ const SkillsList = () => {
           </ul>
         </div>
 
-        <div>
-          <h4 className="text-[var(--white)] font-bold text-lg md:text-xl md:mb-6">Web3</h4>
+        <div
+          data-aos="fade-zoom-in"
+          data-aos-easing="ease-in-back"
+          data-aos-delay="600"
+          data-aos-offset="0"
+        >
+          <h4 className="text-[var(--white)] font-bold text-lg text-left md:text-xl md:mb-4">Web3</h4>
           <ul className="space-y-4 mt-4 text-lg">
             {Object.entries(blockchain).map(([category, items]) => (
               <li key={category} className="w-full">
                 <div
                   onClick={() => toggleItem(category)}
-                  className="md:w-[300px] w-full bg-[#1414149c] rounded-2xl text-left hover:bg-opacity-80 transition-all border border-[var(--white-icon-tr)] cursor-pointer overflow-hidden"
+                  className="w-full bg-[#1414149c] rounded-2xl text-left hover:bg-opacity-80 transition-all border border-[var(--white-icon-tr)] cursor-pointer overflow-hidden"
                 >
                   <div className="flex items-center gap-3 p-4">
-                    {CategoryIcons[category]}
                     <div className="flex items-center gap-2 flex-grow justify-between">
                       <div className="min-w-0 max-w-[200px] md:max-w-none overflow-hidden">
-                        <span className="block truncate text-[var(--white)] text-lg">
+                        <span className="block truncate text-[var(--white)] text-sm">
                           {category}
                         </span>
                       </div>
@@ -207,20 +203,24 @@ const SkillsList = () => {
           </ul>
         </div>
 
-        <div>
-          <h4 className="text-[var(--white)] font-bold text-lg md:text-xl md:mb-6">DevOps</h4>
+        <div
+          data-aos="fade-zoom-in"
+          data-aos-easing="ease-in-back"
+          data-aos-delay="800"
+          data-aos-offset="0"
+        >
+          <h4 className="text-[var(--white)] font-bold text-lg text-left md:text-xl md:mb-4">DevOps</h4>
           <ul className="space-y-4 mt-4 text-lg">
             {Object.entries(devops).map(([category, items]) => (
               <li key={category} className="w-full">
                 <div
                   onClick={() => toggleItem(category)}
-                  className="md:w-[300px] w-full bg-[#1414149c] rounded-2xl text-left hover:bg-opacity-80 transition-all border border-[var(--white-icon-tr)] cursor-pointer overflow-hidden"
+                  className="w-full bg-[#1414149c] rounded-2xl text-left hover:bg-opacity-80 transition-all border border-[var(--white-icon-tr)] cursor-pointer overflow-hidden"
                 >
                   <div className="flex items-center gap-3 p-4">
-                    {CategoryIcons[category]}
                     <div className="flex items-center gap-2 flex-grow justify-between">
                       <div className="min-w-0 max-w-[200px] md:max-w-none overflow-hidden">
-                        <span className="block truncate text-[var(--white)] text-lg">
+                        <span className="block truncate text-[var(--white)] text-sm">
                           {category}
                         </span>
                       </div>
